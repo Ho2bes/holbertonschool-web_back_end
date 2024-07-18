ec5ec913482b2895d47e2e02601ebd10bf6ff980
Ho2bes <8696@holbertonstudents.com>
2024-07-18 06:33:16 +0200

    add set and condition for task 6 and export default task 9

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,13 +1,23 @@
-import Building from './5-building.js';
+import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft must be a number');
+    }
+    if (typeof floors !== 'number') {
+      throw new TypeError('floors must be a number');
+    }
     this._floors = floors;
   }
 
   get floors() {
     return this._floors;
+  }
+
+  set floors(newfloors) {
+    this._floors = newfloors;
   }
 
   evacuationWarningMessage() {
