7bd128b0a2c96dae3af983fec21b19bd99252a36
Ho2bes <8696@holbertonstudents.com>
2024-07-18 06:06:37 +0200

    add type error and condition

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,26 +1,34 @@
-export default class Currency {
-  constructor(code, name) {
-    this._code = code;
+export default class HolbertonCourse {
+  constructor(name, length, students) {
     this._name = name;
-  }
-
-  get code() {
-    return this._code;
-  }
-
-  set code(newcode) {
-    this._code = newcode;
+    this._length = length;
+    this._students = students;
   }
 
   get name() {
     return this._name;
   }
 
-  set name(newname) {
-    this._name = newname;
+  set name(value) {
+    if (typeof value !== 'string') throw new TypeError('Name must be a string');
+    this._name = value;
   }
 
-  displayFullCurrency() {
-    return `${this._name} (${this._code})`;
+  get length() {
+    return this._length;
+  }
+
+  set length(value) {
+    if (typeof value !== 'number') throw new TypeError('Length must be a number');
+    this._length = value;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(value) {
+    if (!Array.isArray(value)) throw new TypeError('Students must be an array of strings');
+    this._students = value;
   }
 }
