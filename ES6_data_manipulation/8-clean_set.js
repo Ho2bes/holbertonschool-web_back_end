9013a49d152f13d0aa7305e24d8fbbbbf19d1835
Ho2bes <8696@holbertonstudents.com>
2024-07-22 15:25:27 +0200

    modification of code 8 about the method

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,7 @@
-export function cleanSet(set, startString) {
-  if (!startString || typeof startString !== 'string') {
-    return '';
-  }
-  return [...set].filter((string) => string && string.startsWith(startString)).map((string) => string.slice(startString.length)).join('-');
+export default function cleanSet(set, startString) {
+  if (!startString || typeof startString !== 'string') return '';
+  return Array.from(set)
+  .filter((value) => typeof value === 'string' && value.startsWith(startString))
+  .map((value) => value.slice(startString.length))
+  .join('-');
 }
