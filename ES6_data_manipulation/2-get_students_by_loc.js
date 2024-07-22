64775f3d9fe77accf0b4a6d1a86b068336d65a59
Ho2bes <8696@holbertonstudents.com>
2024-07-22 10:41:03 +0200

    add condition

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,6 @@
-export function getStudentsByLocation(array, city) {
-  return array.filter((student) => student.location === city);
+export default function getStudentsByLocation(student, city) {
+  if (!Array.isArray(student)) {
+    return [];
+  }
+  return student.filter((student) => student.location === city);
 }
