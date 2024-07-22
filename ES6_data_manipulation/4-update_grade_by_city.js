1dc96d57a9098df72a6106eb5816893e9b38c5ba
Ho2bes <8696@holbertonstudents.com>
2024-07-22 14:39:27 +0200

    add code 4 and 5

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,10 +2,10 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
   return students
     .filter((student) => student.location === city)
     .map((student) => {
-      const grade = newGrades.filter((newGrade) => newGrade.studentId === student.id);
+      const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
       return {
         ...student,
-        grade: grade.length === 0 ? 'N/A' : grade[0].grade,
+        grade: gradeObj ? gradeObj.grade : 'N/A',
       };
     });
 }
