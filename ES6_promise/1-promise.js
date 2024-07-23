828c2a9dc6c763eeae6cc7c131885c2c522ff391
Ho2bes <8696@holbertonstudents.com>
2024-07-23 16:08:06 +0200

    fix indentation

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,13 +1,9 @@
 export default function getFullResponseFromAPI(success) {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     if (success) {
-      resolve({
-        status: 200,
-        body: 'Success',
-      });
+      resolve({ status: 200, body: 'Success' });
     } else {
-      resolve(new Error('The fake API is not working currently'));
+      reject(new Error('The fake API is not working currently'));
     }
   });
 }
-
