a8598dea5d72c1d4ffa654fd8b852230c120f7ee
Ho2bes <8696@holbertonstudents.com>
2024-08-15 15:08:22 +0200

    add all files and code and annotations

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""sum_list function that takes a list input_list of floats as argument"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """sum_list function"""
+    return sum(input_list)
