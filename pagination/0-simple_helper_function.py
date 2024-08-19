dfcba3a9b51c56947b69edb52fb0b3915f4e8e44
Ho2bes <8696@holbertonstudents.com>
2024-08-19 13:13:14 +0200

    add file 0 and 1

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""Simple helper function module for Python async comprehension exercise"""
+
+def index_range(page: int, page_size: int) -> tuple:
+    """Return a tuple of size two containing a start index and an end index"""
+    return ((page - 1) * page_size, page * page_size)
