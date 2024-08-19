de8d5b3758ebcb8aa7d725c88200113dc9d6f486
Ho2bes <8696@holbertonstudents.com>
2024-08-19 10:30:40 +0200

    add async number

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -7,4 +7,5 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> typing.List[float]:
     """Collect 10 random numbers using async comprehensin gasync_generator"""
+    random_numbers = [number async for number in async_generator()]
     return [number async for number in async_generator()]
