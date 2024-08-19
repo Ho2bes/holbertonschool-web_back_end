2ff37d4dc5ee629e79dd52226ea01f10b4f80f40
Ho2bes <8696@holbertonstudents.com>
2024-08-19 13:25:57 +0200

    add dataset and class server

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,15 +1,37 @@
 #!/usr/bin/env python3
-"""Simple helper function module for Python async comprehension exercise"""
+"""method takes 2 integer arguments page"""
+
+import csv
+from typing import List
 
 
-def index_range(page: int, page_size: int) -> tuple:
-    """Return a tuple of size two containing a start index and an end index"""
-    return ((page - 1) * page_size, page * page_size)
+index_range = __import__('0-simple_helper_function').index_range
 
 
-def get_page(page: int = 1, page_size = 10) -> tuple:
-    """Return a tuple of the list of items and the range of the page"""
-    assert isinstance(page, int) and page > 0
-    assert isinstance(page_size, int) and page_size > 0
-    start, end = index_range(page, page_size)
-    return (list(range(start, end)), (start, end))
+class Server:
+    """Server class to paginate database of popular baby names."""
+
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """return the appropriate page of the dataset"""
+
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        start, end = index_range(page, page_size)
+
+        return self.dataset()[start:end]
