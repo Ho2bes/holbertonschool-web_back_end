e4979f38341226892a96cefb99a4237fe849cfcd
Ho2bes <8696@holbertonstudents.com>
2024-08-19 13:19:43 +0200

    add get_page method

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """Simple helper function module for Python async comprehension exercise"""
 
+
 def index_range(page: int, page_size: int) -> tuple:
     """Return a tuple of size two containing a start index and an end index"""
     return ((page - 1) * page_size, page * page_size)
 
-@get_page
+
 def get_page(page: int = 1, page_size = 10) -> tuple:
     """Return a tuple of the list of items and the range of the page"""
     assert isinstance(page, int) and page > 0
