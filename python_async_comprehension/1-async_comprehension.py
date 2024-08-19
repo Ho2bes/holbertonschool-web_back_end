62703f8c1d3bb2ede55e2ce0d7fc5ffe54f83b37
Ho2bes <8696@holbertonstudents.com>
2024-08-19 10:33:24 +0200

    change the return line

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-
 """Async comprehension module for Python async comprehension exercise"""
 
 import typing
+
+
 async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> typing.List[float]:
     """Collect 10 random numbers using async comprehensin gasync_generator"""
     random_numbers = [number async for number in async_generator()]
-    return [number async for number in async_generator()]
+    return random_numbers[:10]
