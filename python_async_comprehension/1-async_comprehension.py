9844c05d4b3745fe624896ab52bdfa25ef2e6c35
Ho2bes <8696@holbertonstudents.com>
2024-08-19 10:36:00 +0200

    pycodestyle

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 """Async comprehension module for Python async comprehension exercise"""
 
+
 import typing
 
 
 async_generator = __import__('0-async_generator').async_generator
+
 
 async def async_comprehension() -> typing.List[float]:
     """Collect 10 random numbers using async comprehensin gasync_generator"""
