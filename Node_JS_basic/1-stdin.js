1b74494a1e37b087c39679e4334f9e5d4e4b3ead
Ho2bes <8696@holbertonstudents.com>
2024-09-16 11:40:37 +0200

    try to fixe task 1 and 2

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,14 +3,8 @@ console.log("Welcome to Holberton School, what is your name?");
 
 // Écoute l'entrée standard (stdin) de l'utilisateur
 process.stdin.on('data', (input) => {
-  // Supprime les espaces autour de l'entrée
   const name = input.toString().trim();
-
-  // Affiche le nom de l'utilisateur
   console.log(`Your name is: ${name}`);
-
-  // Fin du programme
-  process.stdin.end();
 });
 
 // Événement pour la fin de l'entrée
