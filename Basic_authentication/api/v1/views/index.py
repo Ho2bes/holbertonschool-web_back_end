4c91759bb3129bc7d10d0f6aab4c7a53707584bd
Ho2bes <8696@holbertonstudents.com>
2025-02-17 11:53:43 +0100

    try to fix index.py

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,19 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """ GET /api/v1/unauthorized
+    Trigger a 401 Unauthorized error
+    """
+    abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    Triggers a 403 Forbidden error
+    """
+    abort(403)
